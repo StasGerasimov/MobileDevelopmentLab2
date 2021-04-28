4b092af9bc577352bb21791e6699cda6bc596f91
StasGerasimov <yourchoiceweb1@gmail.com>
2021-04-28 17:14:16 +0300

    init

---
+++ navigation/AppNavigation.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import AccountScreen from "../components/AccountScreen";
+import ChartScreen from "../components/ChartScreen";
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigation() {
+    return (
+        <Tab.Navigator
+            tabBarOptions={
+                { labelStyle: { paddingBottom: 5 } }}
+        >
+            <Tab.Screen
+                name="General"
+                component={AccountScreen}
+                options={{
+                    tabBarLabel: 'General',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={size} />
+                    ),
+                }}
+            />
+
+            <Tab.Screen
+                name="Charts"
+                component={ChartScreen}
+                options={{
+                    tabBarLabel: 'Charts',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="poll" color={color} size={size} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
